d04143f5e332c559cae6f4d56d564c5206f9b8df
Adrit <axb5095@gmail.com>
2018-11-05 21:05:56 -0500

    my first commit

---
+++ db/seeds.sql
@@ -0,0 +1,12 @@
+use notes_db;
+
+INSERT INTO notes
+  (title, note)
+VALUES
+  ('my notes', 'things to do today: brush teeth, make bed, go to school');
+
+INSERT INTO notes
+  (title, note)
+VALUES
+  ('my second note', 'practicing playing the piano');
+ 
---
+++ server.js
@@ -0,0 +1,65 @@
+var express = require("express");
+var path = require("path");
+var connection = require("./db/connection");
+
+var app = express();
+var PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/notes", function(req, res) {
+  res.sendFile(path.join(__dirname, "notes.html"));
+});
+
+app.get("/assets/js/index.js", function(req, res) {
+    res.sendFile(path.join(__dirname, "/assets/js/index.js"));
+  });
+  
+
+// Get all notes 
+app.get("/api/notes", function(req, res) {
+    console.log("/api/notes");
+    connection.query("SELECT * FROM notes", function(err, dbTables) {
+        console.log(dbTables);
+      res.json(dbTables);
+    });
+  });
+
+
+  
+  // Save a new notes
+  app.post("/api/notes", function(req, res) {
+      connection.query("INSERT INTO notes SET ?", req.body, function(err, result) {
+        if (err) throw err;
+  
+        connection.query("SELECT * FROM notes WHERE id = ?", [result.insertId], function(err, dbTables) {
+          res.json(dbTables[0]);
+        });
+      });
+    });
+  
+  
+  
+  
+  // Delete a note
+  app.delete("/api/notes/:noteId", function(req, res) {
+    connection.query("DELETE FROM notes WHERE ID = ?", req.params.noteId, function(err, result) {
+      if (err) throw err;
+      res.json(result);
+    });
+  });
+
+  
+
+  
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
